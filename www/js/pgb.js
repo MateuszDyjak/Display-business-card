c2f85b2ddacc2958db9bff82337146259f0f53f8
MateuszDyjak <mateuszdyjak5@gmail.com>
2017-03-26 18:52:54 +0200

    edited

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -6,31 +6,16 @@ function onDeviceReady() {
 	navigator.notification.beep(1);
 }
 
-function deviceInfo() {
 
-	info =  'Hi, I am your smartphone :-)' + '\n' +
-			'=====' + '\n' +
-			'Device Name    : '     + device.name     + '\n' +
-			'Device Cordova : '  + device.cordova + '\n' +
-			'Device Platform: ' + device.platform + '\n' +
-			'Device UUID    : '     + device.uuid     + '\n' +
-			'Device Model   : '    + device.model     + '\n' +
-			'Device Version : '  + device.version  + '\n';
-
-
-	navigator.notification.alert(info);
-
-}
 function displayMyData() {
 
-	info= 'Hello! Below you can find some my personal data \n'
+	info= 'To show data please enter the button below\n'
 
 	+ 'Name : Mateusz\n'
 	+ 'Last name : Dyjak\n'
-	+ 'City : Kraków\n'
-	+ 'Settlement : Dąbie\n'
-	+ 'District : Grzegórzki'
-	+ 'email address : mateuszdyjak5@gmail.com' ;
+	+ 'email : mateuszdyjak5@gmail.com\n'
+	+ 'Dean\'s group : KrDUIS 1011\n'
+	+ 'Field of study : Computer Science' ;
 
 navigator.notification.alert(info);
 
